de3fe0b56701abde79eee10a3189735dae5a1141
Amar <amar.vadhia@gmail.com>
2019-07-19 12:49:42 -0700

    add css/images. looks more similar to airbnb

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql');
 
-// let connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'related_listings'
-// });
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'related_listings'
+});
 
-const connection = mysql.createConnection('mysql://root:@database:3306/');
+// const connection = mysql.createConnection('mysql://root:@database:3306/');
 
 connection.connect((err) => {
   if(err){
